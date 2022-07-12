97ae7886b40ea763d24419d24370ff9c14c85a79
Teagrin Forde <teagrinb@gmail.com>
2022-07-12 17:04:58 -0400

    generateMarkdown js functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -16,8 +18,40 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is the description of your project?',
-        name: 'description',
+        message: 'What is the installation instructions of your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information of your project?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines of your project?',
+        name: 'guidelines',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for your project?',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Which licesnse will you be using for your application?',
+        name: 'license',
+        choices: ['Apache', 'Boost', 'BSD', 'CC0'],
+        default: 'Apache'
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'userName',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email',
     }
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license == 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`;
+  } else if (license == 'Boost') {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]`;
+  } else if ( license == 'BSD') {
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`;
+  } else if (license == 'CC0') {
+    return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]`;
+  } else {
+    return '';
+  }
+};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license == 'Apache') {
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license == 'Boost') {
+    return `(https://www.boost.org/LICENSE_1_0.txt)`;
+  } else if ( license == 'BSD') {
+    return `(https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (license == 'CC0') {
+    return `(http://creativecommons.org/publicdomain/zero/1.0/)`;
+  } else {
+    return '';
+  }
+};
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license == 'Apache') {
+    return '';
+  } else if (license == 'Boost') {
+    return '';
+  } else if ( license == 'BSD') {
+    return '';
+  } else if (license == 'CC0') {
+    return '';
+  } else {
+    return '';
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## ${data.description}
+  ## ${data.tableContents}
+  ## ${data.installation}
+  ## ${data.usage}
+  ## ${data.guidelines}
+  ## ${data.test}
+  ## ${data.license}
+  ## ${data.userName}
+  ## ${data.email}
 `;
 }
 
